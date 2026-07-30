ade3b632ebe2da442b178e1f9aae82b29eccc9c9
CleProgrammer <clebsonmengao@hotmail.com>
2026-07-30 10:56:06 -0300

    update

--- assets/javascript/analytics.js
+++ assets/javascript/analytics.js
@@ -1,5 +1,6 @@
-window.dataLayer = window.dataLayer || [];
+/*window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', 'G-83P5JS0PL9');
+*/
